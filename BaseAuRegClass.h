96551a0cf08e93b562023ecb7948d53ad2bd7b9f
Ethan Riley <riley787cmplex@gmail.com>
2015-05-18 14:08:17 +0100

    updated licenses

--- BaseAuRegClass.h
+++ BaseAuRegClass.h
@@ -5,19 +5,29 @@
 #include<boost\regex.hpp>
 #include<iostream>
 
-/*
-	Please credit me if you are using my code 
-	Im working under the GPL3 Licesnse
-	search it up on google when you have the time 
-	if you need this in capital letters for it to be more 'licencey'
-	Here you go:
-		PLEASE CERDIT ME IF YOU ARE USING MY CODE
-		IM WORKING UNDER THE GPL3 LICENCSE
-		SEARCH IT UP ON GOOGLE WHEN YOU HAVE THE TIME
-	All right reseved, Ethan Riley.
-	:)
-*/
 
+/*
+	The MIT License (MIT)
+	Copyright (c) 2015, Ethan Riley <https://github.com/EthanRiley>
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
 
 class AutoRegister {
 public:
